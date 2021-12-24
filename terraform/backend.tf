4a1bbc80f7627bdea82aaa65651b6bfc2e26d15c
Alekseev Andrey <ganers@mail.ru>
2021-12-24 10:14:09 +0500

    7.3. Основы и принцип работы Терраформ

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -7,3 +7,17 @@ terraform {
         dynamodb_table = "terraform-locks"
     }
 }
+
+//terraform {
+//  required_providers {
+//    aws = {
+//      source  = "hashicorp/aws"
+//      version = "~> 3.0"
+//    }
+//  }
+//  backend "s3" {
+//    bucket = "netologytfstate"
+//    key    = "state"
+//    region = "eu-central-1"
+//  }
+//}
